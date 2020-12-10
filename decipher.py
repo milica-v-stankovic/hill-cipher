b1d29575858431f6f20ad238d11d0546c5001dd5
Milica Stankovic <micika27@gmail.com>
2020-12-10 15:23:06 +0100

    /

--- decipher.py
+++ decipher.py
@@ -1,7 +1,22 @@
 from HillCipher import hillCipher
 
-hc = hillCipher.HillCipher([[17, 17, 5], [21, 18, 21], [2, 2, 19]], 26, 3)
+print("=========================")
+print("HILL DECIPHER")
+print("=========================")
 
-word = hc.decipher("ZVTVTMTEQFSWUXVTMQRKL")
+print()
+print()
+print()
 
-print(word)
+print("Please enter the key file you would like to use: (eg. keys/key.text)")
+
+filename = input()
+
+with open(filename, "r") as file:
+    key = [[int(num) for num in line.split(',')] for line in file]
+
+    hc = hillCipher.HillCipher(key, 26, 3)
+
+    word = hc.decipher("ZVTVTMTEQFSWUXVTMQRKL")
+
+    print(word)
